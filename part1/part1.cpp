39a53499c6c1cbb46f1a219d28c4d1fd9b4f5865
mingwwang <mingwwang@tencent.com>
2022-10-29 00:25:40 +0800

    feat:part1内容更新

---
+++ part1/part1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by ming on 2022/10/28.
+//
+#include <iostream>
+using namespace std;
+/*
+ * main 是一个程序的入口，每个程序有且仅有一个main入口
+ */
+int main(){
+    // 输出 hello World
+    cout << "hello world" << endl;
+    return  0;
+}
